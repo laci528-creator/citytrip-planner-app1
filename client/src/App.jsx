e3f3d3b134f71831a48ac932c6c026356f616e38
Laszlo Haraszti <laci528@gmail.com>
2026-08-30 23:02:48 +0200

    chore: remove unused Vite template styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,6 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 
-import './App.css'
-
 function App() { 
   return ( 
     <BrowserRouter>
